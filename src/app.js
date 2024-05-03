a7be316d191ed0faf3ccdd067bb946987ef68bb2
Knightlife27 <143671996+Knightlife27@users.noreply.github.com>
2024-05-03 18:01:44 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function createCard() {
+  const rankList = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const suits = ["spades", "hearts", "diamonds", "clubs"];
+
+  const getRandomInt = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+
+  let card = document.querySelector(".card");
+  let rank = document.querySelector(".rank");
+
+  card.classList.add(suits[getRandomInt(suits)]);
+  rank.innerHTML = rankList[getRandomInt(rankList)];
 };
